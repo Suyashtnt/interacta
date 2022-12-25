864c07ba14709e9e72ae67b17a1f3f460e4994e4
Suyashtnt <suyashtnt@gmail.com>
2022-12-25 10:49:43 +0200

    Update packages and add unocss

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import UnoCSS from 'unocss/vite'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		UnoCSS({
+			mode: 'svelte-scoped'
+		}),
+		sveltekit()
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
